1c9b34caef655793b497a99fc5c0fa442e769878
Kred88 <sr_aximili@yahoo.it>
2015-10-15 10:53:13 +0200

    Update TL.cs

--- TLSharp.Core/MTProto/TL.cs
+++ TLSharp.Core/MTProto/TL.cs
@@ -272,7 +272,8 @@ namespace TLSharp.Core.MTProto
 		audioEmpty,
 		audio,
 		documentEmpty,
-		document
+		document,
+        disabledFeature
 	}
 
 	public class TL
@@ -1525,9 +1526,10 @@ namespace TLSharp.Core.MTProto
 			return new DcOptionConstructor(id, hostname, ip_address, port);
 		}
 
-		public static Config config(int date, bool test_mode, int this_dc, List<DcOption> dc_options, int chat_size_max)
-		{
-			return new ConfigConstructor(date, test_mode, this_dc, dc_options, chat_size_max);
+        
+        public static Config config(int date, int expires, bool test_mode, int this_dc, List<DcOption> dc_options, int chat_big_size, int chat_size_max, int broadcast_size_max, List<DisabledFeature> disabled_features)
+        {
+			return new ConfigConstructor(date, expires, test_mode, this_dc, dc_options, chat_big_size, chat_size_max, broadcast_size_max, disabled_features);
 		}
 
 		public static NearestDc nearestDc(string country, int this_dc, int nearest_dc)
@@ -2434,15 +2436,20 @@ namespace TLSharp.Core.MTProto
 
 	}
 
-	public abstract class ContactSuggested : TLObject
-	{
+    public abstract class ContactSuggested : TLObject
+    {
 
-	}
+    }
 
-	// types implementations
+    public abstract class DisabledFeature : TLObject
+    {
+
+    }
+
+    // types implementations
 
 
-	public class ErrorConstructor : Error
+    public class ErrorConstructor : Error
 	{
 		public int code;
 		public string text;
@@ -10923,24 +10930,35 @@ namespace TLSharp.Core.MTProto
 	public class ConfigConstructor : Config
 	{
 		public int date;
+        public int expires;
 		public bool test_mode;
 		public int this_dc;
 		public List<DcOption> dc_options;
-		public int chat_size_max;
+        public int chat_big_size;
+        public int chat_size_max;
+        public int broadcast_size_max;
+        public List<DisabledFeature> disabled_features;
 
-		public ConfigConstructor()
+        
+
+
+        public ConfigConstructor()
 		{
 
 		}
 
-		public ConfigConstructor(int date, bool test_mode, int this_dc, List<DcOption> dc_options, int chat_size_max)
-		{
+		public ConfigConstructor(int date, int expires, bool test_mode, int this_dc, List<DcOption> dc_options, int chat_big_size, int chat_size_max, int broadcast_size_max, List<DisabledFeature> disabled_features)
+		{                                                                                                                                             
 			this.date = date;
+            this.expires = expires;
 			this.test_mode = test_mode;
 			this.this_dc = this_dc;
 			this.dc_options = dc_options;
-			this.chat_size_max = chat_size_max;
-		}
+            this.chat_big_size = chat_big_size;
+            this.chat_size_max = chat_size_max;
+            this.broadcast_size_max = broadcast_size_max;
+            this.disabled_features = disabled_features;
+        }
 
 
 		public override Constructor Constructor
@@ -10950,24 +10968,39 @@ namespace TLSharp.Core.MTProto
 
 		public override void Write(BinaryWriter writer)
 		{
-			writer.Write(0x232d5905);
-			writer.Write(this.date);
-			writer.Write(this.test_mode ? 0x997275b5 : 0xbc799737);
+            
+            writer.Write(0x7dae33e0); //old config: 232d5905
+            writer.Write(this.date);
+            writer.Write(this.expires);
+            writer.Write(this.test_mode ? 0x997275b5 : 0xbc799737);
 			writer.Write(this.this_dc);
+
 			writer.Write(0x1cb5c415);
 			writer.Write(this.dc_options.Count);
 			foreach (DcOption dc_options_element in this.dc_options)
 			{
 				dc_options_element.Write(writer);
 			}
-			writer.Write(this.chat_size_max);
-		}
+
+            writer.Write(this.chat_big_size);
+            writer.Write(this.chat_size_max);
+            writer.Write(this.broadcast_size_max);
+
+            writer.Write(0x1cb5c415);
+            writer.Write(this.disabled_features.Count);
+            foreach (DisabledFeature disabled_features_element in this.disabled_features)
+            {
+                disabled_features_element.Write(writer);
+            }
+        }
 
 		public override void Read(BinaryReader reader)
 		{
 			this.date = reader.ReadInt32();
-			this.test_mode = reader.ReadUInt32() == 0x997275b5;
+            this.expires = reader.ReadInt32();
+            this.test_mode = reader.ReadUInt32() == 0x997275b5;
 			this.this_dc = reader.ReadInt32();
+
 			reader.ReadInt32(); // vector code
 			int dc_options_len = reader.ReadInt32();
 			this.dc_options = new List<DcOption>(dc_options_len);
@@ -10977,18 +11010,30 @@ namespace TLSharp.Core.MTProto
 				dc_options_element = TL.Parse<DcOption>(reader);
 				this.dc_options.Add(dc_options_element);
 			}
-			this.chat_size_max = reader.ReadInt32();
-		}
+
+            this.chat_big_size = reader.ReadInt32();
+            this.chat_size_max = reader.ReadInt32();
+            this.broadcast_size_max = reader.ReadInt32();
+
+            reader.ReadInt32(); // vector code
+            int disabled_features_len = reader.ReadInt32();
+            this.disabled_features = new List<DisabledFeature>(disabled_features_len);
+            for (int disabled_features_index = 0; disabled_features_index < disabled_features_len; disabled_features_index++)
+            {
+                DisabledFeature disabled_features_element;
+                disabled_features_element = TL.Parse<DisabledFeature>(reader);
+                this.disabled_features.Add(disabled_features_element);
+            }
+        }
 
 		public override string ToString()
 		{
-			return String.Format("(config date:{0} test_mode:{1} this_dc:{2} dc_options:{3} chat_size_max:{4})", date, test_mode,
-				this_dc, Serializers.VectorToString(dc_options), chat_size_max);
+			return String.Format("(config date:{0} expires:{1} test_mode:{2} this_dc:{3} dc_options:{4} chat_big_size:{5} chat_size_max:{6} broadcast_size_max:{7} disabled_features:{8})",
+                date, expires, test_mode, this_dc, Serializers.VectorToString(dc_options), chat_big_size, chat_size_max, broadcast_size_max, Serializers.VectorToString(disabled_features));
 		}
 	}
 
-
-	public class NearestDcConstructor : NearestDc
+    public class NearestDcConstructor : NearestDc
 	{
 		public string country;
 		public int this_dc;
@@ -14819,4 +14864,49 @@ namespace TLSharp.Core.MTProto
 					id, access_hash, user_id, date, file_name, mime_type, size, thumb, dc_id);
 		}
 	}
+
+
+    public class DisabledFeatureConstructor : DisabledFeature
+    {
+        public string feature;
+        public string description;
+
+        public DisabledFeatureConstructor()
+        {
+
+        }
+
+        public DisabledFeatureConstructor(string feature, string description)
+        {
+            this.feature = feature;
+            this.description = description;
+        }
+
+
+        public override Constructor Constructor
+        {
+            get { return Constructor.disabledFeature; }
+        }
+
+        public override void Write(BinaryWriter writer)
+        {
+            writer.Write(0xae636f24);
+            Serializers.String.write(writer, this.feature);
+            Serializers.String.write(writer, this.description);
+        }
+
+        public override void Read(BinaryReader reader)
+        {
+            this.feature = Serializers.String.read(reader);
+            this.description = Serializers.String.read(reader);
+        }
+
+        public override string ToString()
+        {
+            return
+                String.Format(
+                    "(feature:{0} description:{1}",
+                    feature, description);
+        }
+    }
 }
